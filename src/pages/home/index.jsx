7ceeb3a5a9581fb40f40a21cead26f48fa74af75
MatheusLopesLemos <m.lopeslemos96@gmail.com>
2025-01-07 19:24:43 -0300

    Configuração do bootstrap adicionada

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,11 @@
 
+import { Button } from 'react-bootstrap';
 
 const index = () => {
   return (
     <div>
-      
+      Home
+      <Button variant="primary">Primary</Button>
     </div>
   )
 }
